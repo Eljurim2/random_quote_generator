d7881203271d816f0745b78a2ee95b4ed6c42b64
Eljurim2 <meljurim@gmail.com>
2020-03-03 23:11:54 -0600

    Add the random quote generator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {random} from 'lodash';
+import 'typeface-roboto';
+import {Grid} from '@material-ui/core';
+import {withStyles} from '@material-ui/core/styles';
+import QuoteMachine from './Components/QuoteMachine';
 
-function App() {
+const styles = {
+  container: {
+    display: 'flex',
+    height: '100vh',
+    alignItems: 'center'
+  }
+};
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state ={
+      quotes: [],
+      selectedQuoteIndex: null,
+    }
+    this.assignNewQuoteindex = this.assignNewQuoteindex.bind(this);
+    this.generateNewQuoteIndex = this.generateNewQuoteIndex.bind(this);
+  }
+
+  componentDidMount() {
+
+    fetch("https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json")
+      .then(data => data.json())
+      .then(quotes => this.setState({quotes}, this.assignNewQuoteindex));
+
+  }
+
+  get selectedQuote() {
+    if (!this.state.quotes.length || !Number.isInteger(this.state.selectedQuoteIndex)) {
+      return undefined;
+    }
+    return this.state.quotes[this.state.selectedQuoteIndex];
+  }
+
+/*
+*Returns an integer representing an index in state.quotes
+*If state.quotes is empty, returns undefined
+*/
+  generateNewQuoteIndex() {
+    if(!this.state.quotes.length) {
+      return undefined;
+    }
+    return random(0, this.state.quotes.length - 1);
+  }
+
+  assignNewQuoteindex() {
+    this.setState({selectedQuoteIndex: this.generateNewQuoteIndex() })
+
+  }
+
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid className = {this.props.classes.container} id="quote-box" justify="center" container>
+      <Grid xs = {11} lg = {8} item>
+      {
+        this.selectedQuote ?
+          <QuoteMachine selectedQuote={this.selectedQuote} assignNewQuoteindex={this.assignNewQuoteindex} />
+          : null}
+      </Grid>
+    </Grid>
   );
 }
+}
 
-export default App;
+export default withStyles(styles)(App);
---
+++ src/Components/QuoteMachine.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faTwitter} from '@fortawesome/free-brands-svg-icons';
+
+
+const QuoteMachine = ({assignNewQuoteindex, selectedQuote}) => (
+  <Card>
+    <CardContent>
+
+          <Typography id="text">
+            {selectedQuote.quote} - <span id = "author">{selectedQuote.author}</span>
+          </Typography>
+
+      </CardContent>
+      <CardActions>
+        <Button id = "new-quote" size = "small" onClick={assignNewQuoteindex}>New Quote</Button>
+        <IconButton
+         id = "tweet-quote"
+         target = "_blank"
+         href = {encodeURI(`https://twitter.com/intent/tweet?text=${selectedQuote.quote}&hashtags=QuoteOfTheDay`)}
+         >
+          <FontAwesomeIcon icon = {faTwitter} size = "md"></FontAwesomeIcon>
+        </IconButton>
+      </CardActions>
+  </Card>
+);
+
+export default QuoteMachine
