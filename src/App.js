83ca6acfca6cc120e6d1c5d537ae41a5a4807284
Eljurim2 <meljurim@gmail.com>
2020-03-04 00:22:47 -0600

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {random} from 'lodash';
 import 'typeface-roboto';
-import Grid from '@material-ui/core/Grid';
+import {Grid} from '@material-ui/core';
 import {withStyles} from '@material-ui/core/styles';
 import QuoteMachine from './Components/QuoteMachine';
 
